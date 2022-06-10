a0dbb16db03cbfd60e74718c0c31276578042375
hanr1993 <92591105+hanr1993@users.noreply.github.com>
2022-06-10 16:25:25 +0100

    added styling to dictionary page

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Results from "./results";
 import axios from "axios";
+import "./dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
