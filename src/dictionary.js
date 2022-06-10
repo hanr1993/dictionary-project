41962c2bfecd28d75ce732c719e2a992fecac2d7
hanr1993 <92591105+hanr1993@users.noreply.github.com>
2022-06-10 16:27:27 +0100

    removed dictionary css

--- src/dictionary.js
+++ src/dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary() {
   }
 
   return (
-    <div className="dictionary">
+    <div className="text-center">
       <form onSubmit={search}>
         <input type="Search" onChange={handleKeyword} />
       </form>
